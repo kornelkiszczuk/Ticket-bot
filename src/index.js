3eb29b45ec1937b124afa25178fde1619bea93a5
Sziru <korneloo123@gmail.com>
2023-04-04 00:39:00 +0200

    reverse messages fix

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ client.on('ready', async (client) => {
 })
 
 const userChannelData = new Map();
+const ticketCategory = new Map();
 // do zapisywania id usera ktory założył kanał
 
 client.on("interactionCreate", async (interaction) => {
@@ -69,6 +70,10 @@ client.on("interactionCreate", async (interaction) => {
             content: `Kanał został stworzony! Klinkij, aby przejść: ${channel.toString()}`,
             ephemeral: true,
         });
+
+        // ponizej zapisuje sie nazwa kategorii kanału która będzie wykorzystywana do przesłania w transkrpyt
+        ticketCategory.set(channel.id, interaction.values)
+
         // poniżej zapisuje użytkownika
         userChannelData.set(channel.id, user.id)
         const embed = new EmbedBuilder()
@@ -183,11 +188,13 @@ client.on('interactionCreate', async interaction => {
             const transcriptChannel = client.channels.cache.get("1092099439725903912")
             // ponizej fetchowanie zapisanego uzytkownika zakladajacego ticket
             const getUserId = userChannelData.get(channel.id)
+            // ponizej fetchowanie kategorii ticketa
+            const getCategoryName = ticketCategory.get(channel.id)
             const userOfTicket = await client.users.fetch(getUserId);
             const messages = await channel.messages.fetch();
             const transcript = [];
 
-            messages.forEach(msg => {
+            messages.reverse().forEach(msg => {
                 const timestamp = msg.createdTimestamp;
                 const date = new Date(timestamp);
 
@@ -205,97 +212,7 @@ client.on('interactionCreate', async interaction => {
                 transcript.push(line);
             });
 
-            const htmlCode = `<html>
-            <head>
-                <meta name="viewport" content="width=device-width">
-                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-                <style>
-                    * {
-                        box-sizing: border-box;
-                        margin: 0;
-                        padding: 0;
-                    }
-
-                    html {
-                        font-size: 62.5%;
-                    }
-
-                    body {
-                        font-family: 'Roboto', sans-serif;
-                        background-color: #36393E;
-                        color: white;
-                        line-height: 1;
-                    }
-
-
-                    p {
-                        font-size: 1.6rem;
-                    }
-
-                    .ticket-header {
-                        display: flex;
-                        align-items: center;
-                        gap: 2rem;
-                        padding: 1.2rem 1rem;
-                        border-bottom: 1px solid rgba(79, 84, 92, 0.48);
-                    }
-
-                    .ticket-header p {
-                        margin-bottom: 1.2rem;
-                    }
-
-                    .ticket-header p:nth-last-child(1) {
-                        margin-bottom: 0px;
-                    }
-
-                    .ticket-header .ticket-header-title {
-                        font-size: 2.5rem;
-                        font-weight: 500;
-                    }
-
-                    .ticket-message {
-                        height: 50px;
-                        margin-top: 1rem;
-                        padding-inline: 1rem;
-                        display: flex;
-                        align-items: center;
-                        gap: 20px;
-                    }
-
-                    .ticket-message:hover {
-                        background-color: #32353A;
-                    }
-
-                    .ticket-message .ticket-message-content-info {
-                        display: flex;
-                        align-items: center;
-                        gap: .5rem;
-                        margin-bottom: 1rem;
-                    }
-
-                    .ticket-message .ticket-message-content-info-author {
-                        font-weight: 500;
-                    }
-
-                    .ticket-message .ticket-message-content-info-date {
-                        font-size: 1.2rem;
-                    }
-                </style>
-            </head>
-
-            <body>
-                <div class="ticket-header">
-                    <img class="ticket-header-logo" src="https://i.imgur.com/iZUkMPH.png" width="75px">
-                    <div class="ticket-header-text">
-                        <p class="ticket-header-title">Heaven Project</p>
-                        <p class="ticket-header-name">#ticket-${userOfTicket.tag}</p>
-                        <p class="ticket-user-id">${userOfTicket.id}</p>
-                    </div>
-                </div>
-                ${transcript.join('\n')}
-            </body>
-            </html>
-            `
+            const htmlCode = `<html><head><meta name="viewport" content="width=device-width"><link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet"><style>.ticket-header,.ticket-message{align-items:center;display:flex}*{box-sizing:border-box;margin:0;padding:0}html{font-size:62.5%}body{font-family:Roboto,sans-serif;background-color:#36393e;color:#fff;line-height:1}::-webkit-scrollbar{width:7px;background-color:#2b2d31}::-webkit-scrollbar-thumb{background-color:#1a1b1e;border-radius:5px}p{font-size:1.6rem}.ticket-header{gap:2rem;padding:1.2rem 1rem;border-bottom:1px solid rgba(79,84,92,.48)}.ticket-header p{margin-bottom:1.2rem}.ticket-header p:last-child{margin-bottom:0}.ticket-header .ticket-header-title{font-size:2.5rem;font-weight:500}.ticket-message{min-height:50px;margin-top:1rem;padding-inline:1rem;gap:20px}.ticket-message:hover{background-color:#32353a}.ticket-message .ticket-message-content-info{display:flex;align-items:center;gap:.5rem;margin-bottom:1rem}.ticket-message .ticket-message-content-info-author{font-weight:500}.ticket-message .ticket-message-content-info-date{font-size:1.2rem}</style></head><body><div class="ticket-header"><img class="ticket-header-logo" src="https://i.imgur.com/iZUkMPH.png" width="75px"><div class="ticket-header-text"><p class="ticket-header-title">Heaven Project</p><p class="ticket-header-name">#${getCategoryName}-${userOfTicket.tag}</p><p class="ticket-user-id">${userOfTicket.id}</p></div></div>${transcript.join('\n')}</body></html>`
             const attachment = new AttachmentBuilder(Buffer.from(htmlCode), { name: 'transcript.html' });
             await transcriptChannel.send({ content: `<@${userOfTicket.id}> - ${date.toLocaleString()}`, files: [attachment] })
             try {
